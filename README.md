823d8ab0945e817da1c025113d699528b81d65b9
Andrew Boza <aboza@madmobile.com>
2021-07-10 15:25:41 -0400

    add stuff

---
+++ README.md
@@ -0,0 +1,23 @@
+# @abizzle/eslint-config-abizzle
+
+A shareable config with my preferred plugins and rules.
+
+## Install
+
+```bash
+npm install -D @abizzle/eslint-config-abizzle
+```
+or 
+```bash
+yarn add -D @abizzle/eslint-config-abizzle
+```
+
+## Configure
+
+Within ESLint config:
+
+```json
+{
+  "extends": ["@abizzle/eslint-config-abizzle"]
+}
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["eslint:recommended", "prettier"],
+  plugins: [
+    "sort-destructure-keys",
+    "sort-imports-es6-autofix",
+    "sort-keys-fix",
+    "typescript-sort-keys",
+  ],
+  reportUnusedDisableDirectives: true,
+  rules: {
+    "no-console": ["warn", { allow: ["debug", "warn", "error"] }],
+    "no-var": "warn",
+    "prefer-const": "warn",
+    "sort-destructure-keys/sort-destructure-keys": "warn",
+    "sort-imports-es6-autofix/sort-imports-es6": ["warn"],
+    "sort-keys-fix/sort-keys-fix": "warn",
+    "typescript-sort-keys/interface": "warn",
+    "typescript-sort-keys/string-enum": "warn",
+  },
+};
